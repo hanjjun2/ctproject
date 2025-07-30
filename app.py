d91ae53754aa0f3598704982f874af0fdb8fc07a
hanjjun2 <hanjjun2@gmail.com>
2025-07-30 12:21:02 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,46 +1,45 @@
 import streamlit as st
 import streamlit.components.v1 as htmlviewer
-st.set_page_config(layout='wide', page_title='electronic maze')
 
-# Tilte Msg#1
-st.title('This is Sky Webapp!!')
+st.set_page_config(layout='wide', page_title='JJun2 Webapp')
 
+# Title
+st.title('This is JJun Webapp!!')
+
+# Read HTML files
 with open('./index.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-    f.close()
+    html1 = f.read()
 
-# html = '''
-# <html>
-#     <head>
-#         <title> this is my html </title>
-#     </head>
-       
-#     <body>
-#         <h1>Topic</h1>
-#         <h2>subTopic</h2>
-#     </body>
-# </html>    
-# '''
+with open('./index2.html', 'r', encoding='utf-8') as f:
+    html2 = f.read()
 
-# Box#1(4), Box#2(1)
-col1, col2 = st.columns((4,1))
+with open('./index3.html', 'r', encoding='utf-8') as f:
+    html3 = f.read()
+
+# Layout
+col1, col2 = st.columns((4, 1))
+
+# Main Content Area
 with col1:
-    with st. expander('Content #1 ...'):
+    with st.expander('Content #1 ...'):
         url = 'https://www.youtube.com/watch?v=XyEOEBsa8I4'
-        st. info('Content...')
+        st.info('Content...')
         st.video(url)
 
-with st. expander('Content #2 ...'):
-        #st. write(html, unsafe_allow_html=True)
-        htmlviewer.html(html, height=600)
+    with st.expander('Content #2 ...'):
+        htmlviewer.html(html1, height=1200)
 
-with st. expander('Content #3 ...'):
-        #st. write(html, unsafe_allow_html=True)
-        htmlviewer.html(html, height=600)       
+    with st.expander('Content #3 ...'):
+        htmlviewer.html(html2, height=1200)
 
+    with st.expander('Content #4 ...'):
+        htmlviewer.html(html3, height=1200)
+
+# Side Column
 with col2:
     with st.expander('Tips..'):
-        st. info('Tips..')
-       
-st.markdown('<hr>', unsafe_allow_html=True)       
+        st.info('Tips..')
+
+# Footer
+st.markdown('<hr>', unsafe_allow_html=True)
 st.write('<font color="BLUE">(c)copyright. all rights reserved by jjun', unsafe_allow_html=True)
